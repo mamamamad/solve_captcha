515479809d4c7f41562f72575dfa0c2dfa375bec
mamamamad <mamadhub8@gmail.com>
2024-11-10 07:19:32 -0500

    change api type to post

--- Api.py
+++ Api.py
@@ -1,20 +1,30 @@
-
 from detect_my_picture import detect 
 from fastapi import FastAPI
-from typing import Union
-from decocing_base64 import decoding
+from  pydantic import BaseModel 
+from decocing_base64 import decode_image
+import uvicorn
+
+
 
 app = FastAPI()
 
+class Image(BaseModel):
+    base64 : str
 
 
-@app.get("/solve_captcha")
-def get_code(image_base64: str):
-    decoding(image_base64)
+@app.post("/solve_captcha/")
+def get_code(image: Image):
     
-    captcha_number = detect('SelfMe/captcha/yolov5/picture/picture1.jpg')
+    
+    print(image.base64)
+    print(decode_image(image.base64))
+
+    
+    captcha_number = detect('/home/mmd/vscode/model/solve_captcha/pic/pic.jpg')
     return {"code":captcha_number}
 
+
+uvicorn.run(app,port=3000)
 
 
 
